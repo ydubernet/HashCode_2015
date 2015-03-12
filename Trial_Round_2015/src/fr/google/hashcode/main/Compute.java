e7ffe4b1d8d548fa6d37d75c4ed857fc11c641cf
AlexMili <mili.alexandre@gmail.com>
2015-03-12 20:14:43 +0100

    Ranges compute

--- Trial_Round_2015/src/fr/google/hashcode/main/Compute.java
+++ Trial_Round_2015/src/fr/google/hashcode/main/Compute.java
@@ -2,9 +2,30 @@ package fr.google.hashcode.main;
 
 public class Compute {
 	
-	private datacenter dc;
+	private Datacenter dc;
 	
-	public Compute(datacenter mydc) {
+	public Compute(Datacenter mydc) {
 		this.dc = mydc;
+		populateRanges();
+	}
+	
+	public void populateRanges() {
+		Point[] indispo = dc.getEmplacementIndispo();
+		Range[] ranges = null;
+		
+		for(int j = 0 ; j<dc.getNbRange();j++)
+		{
+			ranges[j] = new Range(dc.getNbEmplacementD(),j);
+			ranges[j].prepare();
+			
+			//Pour chaque range
+			for(int i = 0; i<dc.getNbEmplcementI();i++)
+			{
+				if(indispo[i].x == j)
+				{
+					ranges[j].emplacements[i] = indispo[i];
+				}
+			}
+		}
 	}
 }
