c4dc351526577adca8be6f7e4691174996c892dc
AlexMili <mili.alexandre@gmail.com>
2015-03-12 20:14:36 +0100

    prepare rangée

--- Trial_Round_2015/src/fr/google/hashcode/main/Range.java
+++ Trial_Round_2015/src/fr/google/hashcode/main/Range.java
@@ -1,12 +1,23 @@
 package fr.google.hashcode.main;
 
+import java.util.ArrayList;
+
 public class Range {
 	
-	private int xBegin;
-	private int xEnd;
+	private int taille;
 	private int index;
+	public Point[] emplacements;
 	
-	public Range() {
-		
+	public Range(int size, int ind) {
+		this.taille = size;
+		this.index=ind;
+	}
+	
+	public void prepare()
+	{
+		for(int i =0;i<this.taille;i++)
+		{
+			emplacements[i] = new Point(index, i);
+		}
 	}
 }
