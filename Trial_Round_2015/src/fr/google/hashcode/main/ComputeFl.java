141b6370651e672cd0e5fe864bf0916f4a885aa2
utilisateur <utilisateur@10.11.26.42>
2015-03-12 22:00:03 +0100

    test

--- Trial_Round_2015/src/fr/google/hashcode/main/ComputeFl.java
+++ Trial_Round_2015/src/fr/google/hashcode/main/ComputeFl.java
@@ -3,50 +3,61 @@ package fr.google.hashcode.main;
 public final class ComputeFl {
 
 	public static void Compute(Datacenter dc){
+		int totalemplacement = dc.getNbGroupe()*dc.getNbEmplacementD()-dc.getNbEmplcementI();
+		int currentUsed = 0;
+		boolean next = true;
+		do{
+			Serveur srv = maxServeurAplace(dc.getServeurs());
+			srv.isPlace()
+		} while (next==true)
+		for (int i=0;i<dc.getNbServer();i++){
+			
+		}
 		
 		int[] rangeFirstIndex = new int[dc.getNbRange()];
 		boolean next;
 		int groupe = 0;
-		boolean test = false;
+		//boolean test = false;
+		int maxTaille = getMaxTaille(dc.getServeurs());
 		
 		do{
 			next = false;
-			test = !test;
-			
-			if (test){
-				for (int i = 0 ; i<dc.getNbRange();i++){
-					Serveur srv = maxServeurAplace(dc.getServeurs());
-					for (int col=rangeFirstIndex[i]; col<dc.getNbEmplacementD();col++){
-						if (isPossible(i,col,dc,srv)){
-							srv.range(i, col, groupe);
-							groupe = groupe>=dc.getNbGroupe()-1?0:groupe+1;
-							rangeFirstIndex[i]=col+1+srv.getTaille();
-							next = true;
-							
-							break;
-							
+			//test = !test;
+			for (int taille = maxTaille; taille>0;taille--){
+				//if (test){
+					for (int j = 0 ; j<dc.getNbRange();j++){
+						Serveur srv = maxServeurAplace(dc.getServeurs(),taille);
+						for (int col=rangeFirstIndex[j]; col<dc.getNbEmplacementD();col++){
+							if (isPossible(j,col,dc,srv)){
+								srv.range(j, col, groupe);
+								groupe = groupe>=dc.getNbGroupe()-1?0:groupe+1;
+								rangeFirstIndex[j]=col+1+srv.getTaille();
+								next = true;
+								
+								break;
+								
+							}
 						}
 					}
-				}
-				
-			}
-			else {
-				for (int i = 15 ; i>=0;i--){
-					Serveur srv = maxServeurAplace(dc.getServeurs());
-					for (int col=rangeFirstIndex[i]; col<dc.getNbEmplacementD();col++){
-						if (isPossible(i,col,dc,srv)){
-							srv.range(i, col, groupe);
-							groupe = groupe>=dc.getNbGroupe()-1?0:groupe+1;
-							rangeFirstIndex[i]=col+1+srv.getTaille();
-							next = true;
-							break;
-							
+					
+				/*}
+				else {
+					for (int j = 15 ; j>=0;j--){
+						Serveur srv = maxServeurAplace(dc.getServeurs(),taille);
+						for (int col=rangeFirstIndex[j]; col<dc.getNbEmplacementD();col++){
+							if (isPossible(j,col,dc,srv)){
+								srv.range(j, col, groupe);
+								groupe = groupe>=dc.getNbGroupe()-1?0:groupe+1;
+								rangeFirstIndex[j]=col+1+srv.getTaille();
+								next = true;
+								break;
+								
+							}
 						}
 					}
-				}
-				
+					
+				}*/
 			}
-			
 			
 		} while (next==true);
 		
@@ -83,6 +94,31 @@ public final class ComputeFl {
 		return true;
 	}
 	
+	private static Serveur maxServeurAplace(Serveur[] srv, int taille){
+		Serveur result = srv[0];
+		for (int i = 1 ; i<srv.length;i++){
+			Serveur serveur= srv[i];
+			if (serveur.isPlace()||serveur.getTaille()!=taille)
+				continue;
+			if (serveur.getCapacite()>result.getCapacite()||(serveur.getCapacite()==result.getCapacite()&&serveur.getTaille()<result.getTaille()))
+				result=serveur;
+		}
+			
+		return result;
+	}
+	
+	private static int getMaxTaille(Serveur[] srv){
+		int result = 0;
+		for (int i = 0 ; i<srv.length;i++){
+			if (srv[i].getTaille()>result)
+				result = srv[i].getTaille();
+		}
+			
+			return result;
+		
+	}
+	
+	
 	private static Serveur maxServeurAplace(Serveur[] srv){
 		Serveur result = srv[0];
 		for (int i = 1 ; i<srv.length;i++){
@@ -95,5 +131,6 @@ public final class ComputeFl {
 			
 		return result;
 	}
+	
 
 }
