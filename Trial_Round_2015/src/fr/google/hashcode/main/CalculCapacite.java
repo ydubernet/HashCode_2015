abd3b9c7b10295b4765bd240c3ac98b5aeb0632e
ydubernet <yoadube@laposte.net>
2015-03-12 21:09:59 +0100

    CalculCapacite

--- Trial_Round_2015/src/fr/google/hashcode/main/CalculCapacite.java
+++ Trial_Round_2015/src/fr/google/hashcode/main/CalculCapacite.java
@@ -1,6 +1,8 @@
 package fr.google.hashcode.main;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
 
 
 public class CalculCapacite {
@@ -15,6 +17,50 @@ public class CalculCapacite {
 		
 		return 0;
 	}
+	
+	
+	
+	
+	// Sommer pour chaque groupe tous les éléments de 
+	// chaque rangée correspondant au groupe considéré
+	public int calculCapaciteGroupes(Datacenter dc){
+		Serveur[] serveurs = dc.getServeurs();
+		
+		
+		
+		
+		List<Integer> capaciteGroupes = new ArrayList<Integer>();
+		
+		
+		for(int i = 0 ; i < serveurs.length ; i++){
+			capaciteGroupes.set(serveurs[i].getGroupe(), capaciteGroupes.get(serveurs[i].getGroupe()) + serveurs[i].getCapacite());
+		}
+		
+	
+		
+		
+		// On récupère les capacités selon les groupes et les rangées.
+		for(int i = 0 ; i < serveurs.length ; i++){
+			Serveur s = serveurs[i];
+			
+			int emplacement = s.getEmplacement();
+			int rangee = s.getRange();
+			int capaciteServeur = s.getCapacite();
+			int groupe = s.getGroupe();
+			
+			groupes.add(groupe, capaciteServeur);
+			rangees.add(rangee, capaciteServeur);
+				
+		}
+		return 0;
+	
+	
+	}
+	
+	
+	
+	
+
 	
 	public int calculCapacites(Datacenter dc){
 		Serveur[] serveurs = dc.getServeurs();
@@ -31,10 +77,34 @@ public class CalculCapacite {
 			groupes.add(groupe, capaciteServeur);
 			rangees.add(rangee, capaciteServeur);
 			
+		}
+		
+		
+		// Sommer pour le groupe 1 tous les éléments de chaque rangée correspondant au groupe 1
+		// De même pour chaque groupe
+
+		List<Integer> valeurs = new ArrayList<Integer>();
+		
+		for(Integer g : groupes){
+			int val = 0;
+				
+			val += rangees.get(g);
+
+			valeurs.add(g, val);
+		}
+		
+		// Ensuite, calculer la capacité du 1er groupe sur toutes les rangées sans la rangée où le groupe a calculé le maximum de capacité
+		// Store it
+		// De même pour chaque groupe
+		
+		for(Integer r : rangees){
 			
 		}
 		
 		
+		// A la fin, sortir une capacité garantie minimale de tous les  groupes.
+		
+
 		
 		
 	}
