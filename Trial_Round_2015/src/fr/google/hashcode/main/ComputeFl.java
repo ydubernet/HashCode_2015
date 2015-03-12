8b723733c4313cc3b05815160330e8f599418a62
utilisateur <utilisateur@10.11.26.42>
2015-03-12 20:55:00 +0100

    commit

--- Trial_Round_2015/src/fr/google/hashcode/main/ComputeFl.java
+++ Trial_Round_2015/src/fr/google/hashcode/main/ComputeFl.java
@@ -4,25 +4,73 @@ public final class ComputeFl {
 
 	public static void Compute(Datacenter dc){
 		
-		for (int rang = 0 ; rang<dc.getNbRange();rang++){
+		int[] rangeFirstIndex = new int[dc.getNbRange()];
+		boolean next;
+		int groupe = 0;
+		
+		do{
+			next = false;
+			for (int i = 0 ; i<dc.getNbRange();i++){
+				Serveur srv = maxServeurAplace(dc.getServeurs());
+				for (int col=rangeFirstIndex[i]; col<dc.getNbEmplacementD();col++){
+					if (isPossible(i,col,dc,srv)){
+						srv.range(i, col, groupe);
+						groupe = groupe>=dc.getNbGroupe()?0:groupe+1;
+						if (groupe>46)
+						rangeFirstIndex[i]=col+1;
+						next = true;
+						
+						break;
+						
+					}
+				}
+			}
+		} while (next==true);
+		
+		/*for (int rang = 0 ; rang<dc.getNbRange();rang++){
 			
 			for (int emp=0;emp<dc.getNbEmplacementD();emp++){
 				if (isIndispo(rang,emp,dc.getEmplacementIndispo()))
 					continue;
-				//if ()
+				for (int i = 0;i<dc.getNbServer();i++){
+					Serveur serv = dc.getServeurs()[i];
+					if (serv.isPlace())
+						continue;
+					int diff = dc.getNbEmplacementD()-emp+1;
+					if (serv.getTaille()>)
+				}
+				
 			}
 			
 			
-		}
+		}*/
 		
 	}
 	
-	private static boolean isIndispo(int x, int y, Point[] pointIndispo){
-		for (int i = 0 ;i<pointIndispo.length;i++){
-			if (pointIndispo[i].getX()==x&&pointIndispo[i].getY()==y)
-				return true;
+	private static boolean isPossible(int x, int y, Datacenter dc,Serveur srv){
+		if (dc.getNbEmplacementD()-y<srv.getTaille())
+			return false;
+		
+		for (int i = y ;i<y+srv.getTaille();i++){
+			for (int z = 0 ; z<dc.getNbEmplcementI();z++){
+			if (dc.getEmplacementIndispo()[z].getX()==x&&dc.getEmplacementIndispo()[z].getY()==i)
+				return false;
+			}
 		}
-		return false;
+		return true;
+	}
+	
+	private static Serveur maxServeurAplace(Serveur[] srv){
+		Serveur result = srv[0];
+		for (int i = 1 ; i<srv.length;i++){
+			Serveur serveur= srv[i];
+			if (serveur.isPlace())
+				continue;
+			if (serveur.getCapacite()>result.getCapacite()||(serveur.getCapacite()==result.getCapacite()&&serveur.getTaille()<result.getTaille()))
+				result=serveur;
+		}
+			
+		return result;
 	}
 
 }
