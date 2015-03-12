73d1d5e292a00df227bdd0f12dab22d858fd1c3e
utilisateur <utilisateur@10.11.26.42>
2015-03-12 18:58:41 +0100

    dc.in

---
+++ Trial_Round_2015/src/fr/google/hashcode/main/Serveur.java
@@ -0,0 +1,12 @@
+package fr.google.hashcode.main;
+
+public class Serveur {
+	private int taille;
+	private int capacite;
+
+	public Serveur(int taille, int capacite) {
+		this.taille=taille;
+		this.capacite=capacite;
+	}
+
+}
