04fac58744216c09a511889cd5b3febcae770593
AlexMili <mili.alexandre@gmail.com>
2015-03-12 20:35:05 +0100

    implement emplacement class in Range

--- Trial_Round_2015/src/fr/google/hashcode/main/Range.java
+++ Trial_Round_2015/src/fr/google/hashcode/main/Range.java
@@ -6,7 +6,7 @@ public class Range {
 	
 	private int taille;
 	private int index;
-	public Point[] emplacements;
+	public Emplacement[] emplacements;
 	
 	public Range(int size, int ind) {
 		this.taille = size;
@@ -17,7 +17,17 @@ public class Range {
 	{
 		for(int i =0;i<this.taille;i++)
 		{
-			emplacements[i] = new Point(index, i);
+			emplacements[i] = new Emplacement(new Point(index, i), Emplacement.State.DISPO);
+		}
+	}
+	
+	public int getMaxSize()
+	{
+		int cpt = 0;
+		
+		for(int i = 0;i<taille;i++)
+		{
+			if(emplacements[i])
 		}
 	}
 }
